18cd13f62dc006966c5fe7833b7570030dda70cc
Marcin Zdun <mzdun@midnightbits.com>
2017-03-15 17:49:37 +0100

    Adding --no-pack to help with debugging

--- build/build_linux.py
+++ build/build_linux.py
@@ -6,9 +6,11 @@ import argparse, sys
 parser = argparse.ArgumentParser(description='Builds cmake project for Linux host.')
 parser.add_argument('--clean', action='store_true', default=False, help='removes the build environment before start')
 parser.add_argument('--debug', action='store_true', default=False, help='builds and packs Debug instead of Release')
+parser.add_argument('--no-pack', action='store_true', default=False, help='does not pack at the end of the process')
 args = parser.parse_args()
 
 build = cmake('linux/debug' if args.debug else 'linux/release')
 build.configure('-DCMAKE_BUILD_TYPE={}'.format('Debug' if args.debug else 'Release'), clean=args.clean)
 build.run()
-build.archive(['DEB', 'ZIP', 'TGZ'])
+if not args.no_pack:
+	build.archive(['DEB', 'ZIP', 'TGZ'])
--- build/build_win.py
+++ build/build_win.py
@@ -4,13 +4,15 @@ import argparse
 parser = argparse.ArgumentParser(description='Builds cmake project for Linux host.')
 parser.add_argument('--clean', action='store_true', default=False, help='removes the build environment before start')
 parser.add_argument('--debug', action='store_true', default=False, help='builds and packs Debug instead of Release')
+parser.add_argument('--no-pack', action='store_true', default=False, help='does not pack at the end of the process')
 args = parser.parse_args()
 
-build = cmake('win3\\debug' if args.debug else 'win32\\release')
+build = cmake('win32\\debug' if args.debug else 'win32\\release')
 build.configure(clean=args.clean)
 build.run('--config', 'Debug' if args.debug else 'Release')
 
-if args.debug:
-	build.archive(['ZIP'], '-C', 'Debug')
-else:
-	build.archive(['ZIP'])
+if not args.no_pack:
+	if args.debug:
+		build.archive(['ZIP'], '-C', 'Debug')
+	else:
+		build.archive(['ZIP'])
