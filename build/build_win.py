b00a61d619ca79ec9678f190a5ef39d33e102c28
Marcin Zdun <mzdun@midnightbits.com>
2017-03-23 16:27:49 +0100

    Tweaking the layout. Also, enabling paralel build on Windows

--- build/build_win.py
+++ build/build_win.py
@@ -1,5 +1,6 @@
 from build_any import cmake
 import argparse
+import multiprocessing
 
 parser = argparse.ArgumentParser(description='Builds cmake project for Linux host.')
 parser.add_argument('--clean', action='store_true', default=False, help='removes the build environment before start')
@@ -8,8 +9,8 @@ parser.add_argument('--no-pack', action='store_true', default=False, help='does 
 args = parser.parse_args()
 
 build = cmake('win32\\debug' if args.debug else 'win32\\release')
-build.configure('-DCMAKE_BUILD_TYPE={}'.format('Debug' if args.debug else 'Release'), clean=args.clean)
-build.run('--config', 'Debug' if args.debug else 'Release')
+build.configure('-DCMAKE_BUILD_TYPE={}'.format('Debug' if args.debug else 'Release'), '-G', 'Visual Studio 15 2017', clean=args.clean)
+build.run('--config', 'Debug' if args.debug else 'Release', '--', '/maxcpucount:' + str(multiprocessing.cpu_count()))
 
 if not args.no_pack:
 	if args.debug:
